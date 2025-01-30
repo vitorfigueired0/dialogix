9af8bfc92568d8ae14f6d84f4176ded2c836b100
Vitor Figueiredo <vitorfigueiredodev@outlook.com>
2025-01-30 17:53:57 -0300

    Prisma config; Swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { printAscii } from './util/ascii';
 
-async function bootstrap() {
+async function main() {
+  printAscii()
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Dialogix')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+    
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000); 
 }
-bootstrap();
+
+main();
